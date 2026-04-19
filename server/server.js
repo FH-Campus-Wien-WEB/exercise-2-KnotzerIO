a36dcc3e178eaaff136fe54aebeb4f80c20af44d
Mark <knotzermark@gmail.com>
2026-04-19 20:30:28 +0200

    implement movie retrieval and update endpoints in server

--- server/server.js
+++ server/server.js
@@ -1,36 +1,37 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const movieModel = require('./movie-model.js');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const movieModel = require("./movie-model.js");
 
 const app = express();
 
-// Parse urlencoded bodies
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "files")));
 
-// Serve static content in directory 'files'
-app.use(express.static(path.join(__dirname, 'files')));
+app.get("/movies", function (req, res) {
+  res.json(Object.values(movieModel));
+});
 
-// Configure a 'get' endpoint for all movies..
-app.get('/movies', function (req, res) {
-  /* Task 1.2. Remove the line below and eturn the movies from 
-     the model as an array */
-  res.sendStatus(404)
-})
+app.get("/movies/:imdbID", function (req, res) {
+  const movie = movieModel[req.params.imdbID];
+  if (movie) {
+    res.send(movie);
+  } else {
+    res.sendStatus(404);
+  }
+});
 
-// Configure a 'get' endpoint for a specific movie
-app.get('/movies/:imdbID', function (req, res) {
-  /* Task 2.1. Remove the line below and add the 
-    functionality here */
-  res.sendStatus(404)
-})
+app.put("/movies/:imdbID", function (req, res) {
+  const id = req.params.imdbID;
+  if (movieModel[id]) {
+    movieModel[id] = req.body;
+    res.sendStatus(200);
+  } else {
+    movieModel[id] = req.body;
+    res.status(201).send(req.body);
+  }
+});
 
-/* Task 3.1 and 3.2.
-   - Add a new PUT endpoint
-   - Check whether the movie sent by the client already exists 
-     and continue as described in the assignment */
+app.listen(3000);
 
-app.listen(3000)
-
-console.log("Server now listening on http://localhost:3000/")
-
+console.log("Server now listening on http://localhost:3000/");
