eb9726e8703a1fd62b4463bca1a8635d770058cf
Mark <knotzermark@gmail.com>
2026-04-19 20:32:02 +0200

    format code

--- server/movie-model.js
+++ server/movie-model.js
@@ -1,46 +1,49 @@
 const movies = {
-    tt0245429: {
-        imdbID: 'tt0245429',
-        Title: 'Spirited Away',
-        Released: '2003-03-28',
-        Runtime: 124,
-        Genres: ['Animation', 'Adventure', 'Family'],
-        Directors: ['Hayao Miyazaki'],
-        Writers: ['Hayao Miyazaki'],
-        Actors: ['Miyu Irino', 'Rumi Hiiragi', 'Mari Natsuki'],
-        Plot: 'During her family\'s move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches and spirits, and where humans are changed into beasts.',
-        Poster: 'https://m.media-amazon.com/images/M/MV5BNTEyNmEwOWUtYzkyOC00ZTQ4LTllZmUtMjk0Y2YwOGUzYjRiXkEyXkFqcGc@._V1_SX300.jpg',
-        Metascore: 96,
-        imdbRating: 8.6
-    },
-    tt1285016: {
-        imdbID: 'tt1285016',
-        Title: 'The Social Network',
-        Released: '2010-10-01',
-        Runtime: 120,
-        Genres: ['Biography', 'Drama'],
-        Directors: ['David Fincher'],
-        Writers: ['Aaron Sorkin', 'Ben Mezrich'],
-        Actors: ['Jesse Eisenberg', 'Andrew Garfield', 'Justin Timberlake'],
-        Plot: 'As Harvard student Mark Zuckerberg creates the social networking site that would become known as Facebook, he is sued by the twins who claimed he stole their idea and by the co-founder who was later squeezed out of the business.',
-        Poster: 'https://m.media-amazon.com/images/M/MV5BMjlkNTE5ZTUtNGEwNy00MGVhLThmZjMtZjU1NDE5Zjk1NDZkXkEyXkFqcGc@._V1_SX300.jpg',
-        Metascore: 95,
-        imdbRating: 7.8
-    },
-    tt0137523: {
-        imdbID: 'tt0137523',
-        Title: 'Fight Club',
-        Released: '1999-10-15',
-        Runtime: 139,
-        Genres: ['Crime', 'Drama', 'Thriller'],
-        Directors: ['David Fincher'],
-        Writers: ['Chuck Palahniuk', 'Jim Uhls'],
-        Actors: ['Brad Pitt', 'Edward Norton', 'Meat Loaf'],
-        Plot: 'An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.',
-        Poster: 'https://m.media-amazon.com/images/M/MV5BOTgyOGQ1NDItNGU3Ny00MjU3LTg2YWEtNmEyYjBiMjI1Y2M5XkEyXkFqcGc@._V1_SX300.jpg',
-        Metascore: 67,
-        imdbRating: 8.8
-    }
-}
+  tt0245429: {
+    imdbID: "tt0245429",
+    Title: "Spirited Away",
+    Released: "2003-03-28",
+    Runtime: 124,
+    Genres: ["Animation", "Adventure", "Family"],
+    Directors: ["Hayao Miyazaki"],
+    Writers: ["Hayao Miyazaki"],
+    Actors: ["Miyu Irino", "Rumi Hiiragi", "Mari Natsuki"],
+    Plot: "During her family's move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches and spirits, and where humans are changed into beasts.",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNTEyNmEwOWUtYzkyOC00ZTQ4LTllZmUtMjk0Y2YwOGUzYjRiXkEyXkFqcGc@._V1_SX300.jpg",
+    Metascore: 96,
+    imdbRating: 8.6,
+  },
+  tt1285016: {
+    imdbID: "tt1285016",
+    Title: "The Social Network",
+    Released: "2010-10-01",
+    Runtime: 120,
+    Genres: ["Biography", "Drama"],
+    Directors: ["David Fincher"],
+    Writers: ["Aaron Sorkin", "Ben Mezrich"],
+    Actors: ["Jesse Eisenberg", "Andrew Garfield", "Justin Timberlake"],
+    Plot: "As Harvard student Mark Zuckerberg creates the social networking site that would become known as Facebook, he is sued by the twins who claimed he stole their idea and by the co-founder who was later squeezed out of the business.",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjlkNTE5ZTUtNGEwNy00MGVhLThmZjMtZjU1NDE5Zjk1NDZkXkEyXkFqcGc@._V1_SX300.jpg",
+    Metascore: 95,
+    imdbRating: 7.8,
+  },
+  tt0137523: {
+    imdbID: "tt0137523",
+    Title: "Fight Club",
+    Released: "1999-10-15",
+    Runtime: 139,
+    Genres: ["Crime", "Drama", "Thriller"],
+    Directors: ["David Fincher"],
+    Writers: ["Chuck Palahniuk", "Jim Uhls"],
+    Actors: ["Brad Pitt", "Edward Norton", "Meat Loaf"],
+    Plot: "An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BOTgyOGQ1NDItNGU3Ny00MjU3LTg2YWEtNmEyYjBiMjI1Y2M5XkEyXkFqcGc@._V1_SX300.jpg",
+    Metascore: 67,
+    imdbRating: 8.8,
+  },
+};
 
-module.exports = movies
+module.exports = movies;
