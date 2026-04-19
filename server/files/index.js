9eddd80dbf5b47394f3bd225f8f4e0c1315d5403
Mark <knotzermark@gmail.com>
2026-04-19 20:06:01 +0200

    add code from exercise 1

--- server/files/index.js
+++ server/files/index.js
@@ -1,21 +1,49 @@
 window.onload = function () {
   const xhr = new XMLHttpRequest();
   xhr.onload = function () {
-    const bodyElement = document.querySelector("body");
+    const bodyElement = document.querySelector("main");
     if (xhr.status == 200) {
       const movies = JSON.parse(xhr.responseText);
-      for (const movie of movies) {
-        /* Task 1.3. Add your code from exercise 1 here 
-           and include a non-functional 'Edit' button
-           to pass this test */
-      }
+      movies.forEach((movie) => {
+        const movieEl = document.createElement("article");
+        movieEl.id = movie.imdbID;
 
+        movieEl.innerHTML = /*html*/ `
+                    <img src="${movie.Poster}" alt="${movie.Title}">
+                    <div>
+                        <h3>${movie.Title}</h3>
+                        <p>Runtime: ${movie.Runtime}</p>
+                        <p>Released: ${movie.Released}</p>
+                        <div>
+                            ${movie.Genres.map((genre) => `<span class="genre">${genre}</span>`).join("")}
+                        </div>
+                        <p>${movie.Plot}</p>
+                        <h4>Director</h4>
+                        <ul>
+                            ${movie.Directors.map((d) => `<li>${d}</li>`).join("")}
+                        </ul>
+                        <h4>Writers</h4>
+                        <ul>
+                            ${movie.Writers.map((w) => `<li>${w}</li>`).join("")}
+                        </ul>
+                        <h4>Actors</h4>
+                        <ul>
+                            ${movie.Actors.map((a) => `<li>${a}</li>`).join("")}
+                        </ul>
+                        <button class="edit-btn">Edit</button>
+                    </div>
+                `;
+        movieEl.querySelector(".edit-btn").onclick = function () {
+          location.href = "edit.html?imdbID=" + movie.imdbID;
+        };
+        bodyElement.append(movieEl);
+      });
     } else {
       bodyElement.append(
         "Daten konnten nicht geladen werden, Status " +
           xhr.status +
           " - " +
-          xhr.statusText
+          xhr.statusText,
       );
     }
   };
